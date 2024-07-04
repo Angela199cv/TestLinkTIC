aba0e3374aad749bd5e15244032a00d78da31bb4
Angela <angela.1999.cardenas@gmail.com>
2024-07-04 17:20:35 -0500

    status code is added

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -6,95 +6,156 @@ export const getProducts = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const connection = await connect();
-  const products = await connection.query("SELECT * FROM products");
-  return res.json(products[0]);
+  try {
+    const connection = await connect();
+    const products = await connection.query("SELECT * FROM products");
+    await connection.end();
+    if (products[0].length === 0) {
+      return res.status(404).json({ message: `Products not found` });
+    } else {
+      return res.status(200).json(products[0]);
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
 };
 
 export const getProductById = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const id = req.params.productId;
-  const connection = await connect();
-  connection.query("SELECT * FROM products WHERE id= ?", [id]);
-  return res.json();
+  try {
+    const id = req.params.productId;
+    const connection = await connect();
+    connection.query("SELECT * FROM products WHERE id= ?", [id]);
+    if (!id) {
+      return res.status(404).json({ message: `Products not found` });
+    } else {
+      return res.status(200).json();
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
 };
 
 export const getProductByName = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const name = req.params.productName;
-  const connection = await connect();
-  connection.query("SELECT * FROM products WHERE name= ?", [name]);
-  return res.json();
+  try {
+    const name = req.params.productName;
+    const connection = await connect();
+    connection.query("SELECT * FROM products WHERE name= ?", [name]);
+    if (!name) {
+      return res.status(404).json({ message: `Products not found` });
+    } else {
+      return res.status(200).json();
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
 };
 
 export const createProduct = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const newPost: Post = req.body;
-  const connection = await connect();
-  connection.query("INSERT INTO products SET ?", [newPost]);
-  return res.json({
-    message: "Product Created",
-  });
+  try {
+    const newPost: Post = req.body;
+    const connection = await connect();
+    connection.query("INSERT INTO products SET ?", [newPost]);
+    return res.status(200).json({
+      message: "Product Created",
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
 };
 
 export const deleteProductById = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const id = req.params.productId;
-  const connection = await connect();
-  await connection.query("DELETE FROM products WHERE id= ?", [id]);
-  return res.json({
-    message: `Success deleted ${id}`,
-  });
+  try {
+    const id = req.params.productId;
+    const connection = await connect();
+    await connection.query("DELETE FROM products WHERE id= ?", [id]);
+    if (!id) {
+      return res.status(404).json({ message: `Products not found` });
+    } else {
+      return res.status(200).json({
+        message: `Success deleted ${id}`,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
 };
 
 export const deleteProductByName = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const name = req.params.producName;
-  const connection = await connect();
-  await connection.query("DELETE FROM products WHERE name= ?", [name]);
-  return res.json({
-    message: `Success deleted ${name}`,
-  });
+  try {
+    const name = req.params.producName;
+    const connection = await connect();
+    await connection.query("DELETE FROM products WHERE name= ?", [name]);
+    if (!name) {
+      return res.status(404).json({ message: `Products not found` });
+    } else {
+      return res.status(200).json({
+        message: `Success deleted ${name}`,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
 };
 
 export const updateProductById = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const id = req.params.productId;
-  const updateProductById: Post = req.body;
-  const connection = await connect();
-  await connection.query("UPDATE products set ? WHERE id= ?", [
-    updateProductById,
-    id,
-  ]);
-  return res.json({
-    message: `Success updated ${id}`,
-  });
+  try {
+    const id = req.params.productId;
+    const updateProductById: Post = req.body;
+    const connection = await connect();
+    await connection.query("UPDATE products set ? WHERE id= ?", [
+      updateProductById,
+      id,
+    ]);
+    if (!id) {
+      return res.status(404).json({ message: `Products not found` });
+    } else {
+      return res.status(200).json({
+        message: `Success updated ${id}`,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
 };
 
 export const updateProductByName = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const name = req.params.producName;
-  const updateProductByName: Post = req.body;
-  const connection = await connect();
-  await connection.query("UPDATE products set ? WHERE name= ?", [
-    updateProductByName,
-    name,
-  ]);
-  return res.json({
-    message: `Success updated ${name}`,
-  });
+  try {
+    const name = req.params.producName;
+    const updateProductByName: Post = req.body;
+    const connection = await connect();
+    await connection.query("UPDATE products set ? WHERE name= ?", [
+      updateProductByName,
+      name,
+    ]);
+    if (!name) {
+      return res.status(404).json({ message: `Products not found` });
+    } else {
+      return res.status(200).json({
+        message: `Success updated ${name}`,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
 };
